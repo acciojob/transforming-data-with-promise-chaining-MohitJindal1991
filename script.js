5a70be011c671c3ba382555fc4f84fb9a8f5cf56
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-01 19:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-//your JS code here. If required.
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+    const input = document.querySelector('#ip');
+    const output = document.querySelector("#output");
+
+    // Convert input value to a number
+    const initialValue = parseFloat(input.value);
+
+    // Initial Promise (Resolves after 2 seconds)
+    new Promise((resolve) => {
+        setTimeout(() => {
+            output.innerHTML = `Result: ${initialValue}`;
+            resolve(initialValue);
+        }, 2000);
+    })
+    .then((num) => {
+        // Multiply by 2 (Resolves after 2 seconds)
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                num = num * 2;
+                output.innerHTML = `Result: ${num}`;
+                resolve(num);
+            }, 2000);
+        });
+    })
+    .then((num) => {
+        // Subtract 3 (Resolves after 1 second)
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                num = num - 3;
+                output.innerHTML = `Result: ${num}`;
+                resolve(num);
+            }, 1000);
+        });
+    })
+    .then((num) => {
+        // Divide by 2 (Resolves after 1 second)
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                num = num / 2;
+                output.innerHTML = `Result: ${num}`;
+                resolve(num);
+            }, 1000);
+        });
+    })
+    .then((num) => {
+        // Add 10 (Final transformation, resolves after 1 second)
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                num = num + 10;
+                output.innerHTML = `Final Result: ${num}`;
+                resolve(num);
+            }, 1000);
+        });
+    });
+});
